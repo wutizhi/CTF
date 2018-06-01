1871632a8a5c95f11ea975188d232d2cb0c4d500
wutizhi <38966640+wutizhi@users.noreply.github.com>
2018-06-01 09:50:35 +0800

    Create hello.s

---
+++ Myfristsecurity/hello.s
@@ -0,0 +1,33 @@
+	.file	"hello.c"
+	.globl	x
+	.data
+	.align 4
+	.type	x, @object
+	.size	x, 4
+x:
+	.long	5
+	.section	.rodata
+.LC0:
+	.string	"Hello CTFer"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$.LC0, %edi
+	call	puts
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.9) 5.4.0 20160609"
+	.section	.note.GNU-stack,"",@progbits
